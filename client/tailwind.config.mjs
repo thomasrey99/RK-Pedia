96051b7a52b37fba232d8fb3f4ee0e3394709d9d
thomasRey99 <thomas_rey1999@outlook.com>
2025-01-23 19:17:46 -0300

    initial commit: clean project

---
+++ client/tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+};
